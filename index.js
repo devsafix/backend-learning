040f40ac7afc78ef04f382a263f75633bd47f557
devsafix <devsafix@gmail.com>
2025-03-20 15:53:12 +0600

    Basic Setup Done

---
+++ index.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/twitter", (req, res) => {
+  res.send("Hello Twitter!");
+});
+
+app.get("/login", (req, res) => {
+  res.send("<h1>Hello Login!</h1>");
+});
+
+app.get("/youtube", (req, res) => {
+    res.send("<h2>Hello Youtube!</h2>");
+  });
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
