c9d07d6158bfb985f781d67eb7aa68cf75b901b4
devsafix <devsafix@gmail.com>
2025-03-20 16:16:44 +0600

    Add Json Api

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-require('dotenv').config()
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+
+const json = {
+  userId: 1,
+  id: 1,
+  title:
+    "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+  body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+};
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -16,8 +24,12 @@ app.get("/login", (req, res) => {
 });
 
 app.get("/youtube", (req, res) => {
-    res.send("<h2>Hello Youtube!</h2>");
-  });
+  res.send("<h2>Hello Youtube!</h2>");
+});
+
+app.get("/json", (req, res) => {
+  res.json(json);
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
