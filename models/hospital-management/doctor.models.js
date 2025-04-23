9a479dc989246e8c70c0f77df1b518e208bdbb58
devsafix <devsafix@gmail.com>
2025-04-23 22:55:06 +0600

    PUSH

---
+++ models/hospital-management/doctor.models.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const doctorSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Doctor = mongoose.model("Doctor", doctorSchema);
