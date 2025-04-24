544cca43d84c3af27d1db832189304839b02173e
devsafix <devsafix@gmail.com>
2025-04-24 23:43:11 +0600

    DONE

--- models/hospital-management/hospital.models.js
+++ models/hospital-management/hospital.models.js
@@ -1,7 +1,46 @@
 import mongoose from "mongoose";
 
 const hospitalSchema = new mongoose.Schema(
-  {},
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    website: {
+      type: String,
+      required: true,
+    },
+    departments: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Department",
+      },
+    ],
+    doctors: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Doctor",
+      },
+    ],
+    patients: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Patient",
+      },
+    ],
+  },
   {
     timestamps: true,
   }
