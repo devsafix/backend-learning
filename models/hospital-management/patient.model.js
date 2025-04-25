785313f75c814ff02149d9e3d51c6a0a0a5a124d
devsafix <devsafix@gmail.com>
2025-04-25 23:30:28 +0600

    ODN

--- models/hospital-management/patient.model.js
+++ models/hospital-management/patient.model.js
@@ -1,7 +1,16 @@
 import mongoose from "mongoose";
 
 const patientSchema = new mongoose.Schema(
-  {},
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      required: true,
+    },
+  },
   {
     timestamps: true,
   }
