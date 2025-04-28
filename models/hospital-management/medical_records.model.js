c40994c387cc891585269442ccd5d02dbe21ebdd
devsafix <devsafix@gmail.com>
2025-04-28 23:14:27 +0600

    UPDATE

--- models/hospital-management/medical_records.model.js
+++ models/hospital-management/medical_records.model.js
@@ -1,10 +1,36 @@
 import mongoose from "mongoose";
 
 const medicalRecordSchema = new mongoose.Schema(
-  {},
+  {
+    patientId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Patient",
+      required: true,
+    },
+    doctorId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Doctor",
+      required: true,
+    },
+    diagnosis: {
+      type: String,
+      required: true,
+    },
+    treatment: {
+      type: String,
+      required: true,
+    },
+    dateOfVisit: {
+      type: Date,
+      default: Date.now,
+    },
+  },
   {
     timestamps: true,
   }
 );
 
-export const MedicalRecord = mongoose.model("MedicalRecord", medicalRecordSchema);
+export const MedicalRecord = mongoose.model(
+  "MedicalRecord",
+  medicalRecordSchema
+);
