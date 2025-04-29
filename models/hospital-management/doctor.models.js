34412763bea031674776a74828dea3a9f6534a02
devsafix <devsafix@gmail.com>
2025-04-29 22:48:11 +0600

    UPDATE

--- models/hospital-management/doctor.models.js
+++ models/hospital-management/doctor.models.js
@@ -5,8 +5,15 @@ const doctorSchema = new mongoose.Schema(
     name: {
       type: String,
       required: true,
+
+      trim: true,
     },
-    
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
   },
   {
     timestamps: true,
